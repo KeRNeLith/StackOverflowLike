72b9b6b20a45ad2f205bbdc4f27c6b3e3ebfa17e
Alexandre Raberin <kernelith@live.fr>
2017-02-20 19:15:12 +0100

    Correct a compatibility issue with firefox

--- src/StackOverflowAngularClient/app/controllers/login/loginControllers.js
+++ src/StackOverflowAngularClient/app/controllers/login/loginControllers.js
@@ -7,42 +7,6 @@
 var loginModule = angular.module('segFault.login');
 
 // Define controllers
-class AuthCtrl
-{
-    constructor(UserService, AuthService)
-    {
-        this.UserService = UserService;
-        this.AuthService = AuthService;
-    }
-
-    login()
-    {
-        this.UserService.login(this.username, this.password);
-    }
-
-    register()
-    {
-        this.UserService.register(this.username, this.password);
-    }
-
-    logout()
-    {
-        this.UserService.logout();
-    }
-
-    isAuthenticated()
-    {
-        return this.AuthService.isAuthenticated();
-    }
-
-    currentUser()
-    {
-        return this.AuthService.currentUser();
-    }
-}
-loginModule.controller('AuthCtrl', AuthCtrl);
-
-// Other controllers
 loginModule.controller('LoginCtrl', function($scope, PageService)
 {
     PageService.setTitle('Login - ' + PageService.default());
@@ -55,4 +19,33 @@ loginModule.controller('RegisterCtrl', function($scope, PageService)
     PageService.setTitle('Register - ' + PageService.default());
 
     // TODO
+});
+
+loginModule.controller('AuthCtrl', function (UserService, AuthService) {
+    let self = this;
+
+    self.login = function()
+    {
+        UserService.login(self.username, self.password);
+    };
+
+    self.register = function()
+    {
+        UserService.register(self.username, self.password);
+    };
+
+    self.logout = function()
+    {
+        UserService.logout();
+    };
+
+    self.isAuthenticated = function()
+    {
+        return AuthService.isAuthenticated();
+    };
+
+    self.currentUser = function()
+    {
+        return AuthService.currentUser();
+    };
 });
